bc3ef7da1966a46aa3e85e79922c13ac0940b5ac
kathmarioly <katerinemarioly@gmail.com>
2017-07-07 22:05:35 -0400

    Printed Pokemon

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,3 +30,25 @@ $(document).ready(function () {
 /*Sección buscador*/
 
 
+$(document).ready(function() {
+	$.ajax({
+        url: 'https://pokeapi.co/api/v2/pokemon/',
+        type: 'GET', //tipo de dato que quiero obtener
+        dataType: 'JSON',
+        data: {"limit": '811'}, //cantidad de elementos que se alojan en mi json
+    })
+	.done(function(res) {
+		console.log("success");
+		console.log(res);
+		for (var i = 1; i < 719; i++) {
+			var pokeimg = $("<img src=https://pokeapi.co/media/img/" + i + ".png id=" + i + ">");
+			$("div.pokemons").append(pokeimg);
+		}
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	})
+});
